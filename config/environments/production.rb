16ecf7d21c449b9dd303f87d8f2051875500e087
CB <ybloc1@outlook.com>
2021-08-16 19:20:52 -0400

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  GA.tracker = "UA-205114566-1"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -95,7 +95,5 @@ Rails.application.configure do
   #sendgrid
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: "http://rocketelevators.xyz/" }
-
-  GA.tracker = "UA-205114566-1"
 
 end
